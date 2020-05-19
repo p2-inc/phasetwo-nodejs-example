597b4732ad931fd6dacf56ef1e8cdd9bbef757d2
Soma Holiday <soma@somaholiday.com>
2020-05-19 14:51:28 -0700

    Prettier app.js

--- app.js
+++ app.js
@@ -32,12 +32,14 @@ app.use(cors());
 
 var memoryStore = new session.MemoryStore();
 
-app.use(session({
-  secret: 'some secret',
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore
-}));
+app.use(
+  session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  }),
+);
 
 // Provide the session store to the Keycloak so that sessions
 // can be invalidated from the Keycloak console callback.
@@ -46,24 +48,26 @@ app.use(session({
 // installed from the Keycloak web console.
 
 var keycloak = new Keycloak({
-  store: memoryStore
+  store: memoryStore,
 });
 
-app.use(keycloak.middleware({
-  logout: '/logout',
-  admin: '/'
-}));
+app.use(
+  keycloak.middleware({
+    logout: '/logout',
+    admin: '/',
+  }),
+);
 
 app.get('/service/public', function (req, res) {
-  res.json({message: 'public'});
+  res.json({ message: 'public' });
 });
 
 app.get('/service/secured', keycloak.protect('realm:user'), function (req, res) {
-  res.json({message: 'secured'});
+  res.json({ message: 'secured' });
 });
 
 app.get('/service/admin', keycloak.protect('realm:admin'), function (req, res) {
-  res.json({message: 'admin'});
+  res.json({ message: 'admin' });
 });
 
 app.use('*', function (req, res) {
